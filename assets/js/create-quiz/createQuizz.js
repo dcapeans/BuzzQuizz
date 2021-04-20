9ff13299ca65dadb4abd7ad0f48f3c6122694604
luanerdy <luan.vini1@gmail.com>
2021-04-20 03:49:55 -0300

    Adjusts in Create Quizz Button

--- assets/js/create-quiz/createQuizz.js
+++ assets/js/create-quiz/createQuizz.js
@@ -1,7 +1,7 @@
 function createQuizz() {
     const myQuizzes = document.querySelector('.my-quizzes');
     const allQuizzes = document.querySelector('.all-quizzes');
-    const creation = document.querySelectorAll('.creation-quizz');
+    const creation = document.querySelector('.creation-quizz');
 
-    showOrHide(myQuizzes, allQuizzes, ...creation);
+    showOrHide(myQuizzes, allQuizzes, creation);
 }
