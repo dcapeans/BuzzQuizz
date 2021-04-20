896ed43827d478f2dd39505e7436ff63bfc8afe9
luanerdy <luan.vini1@gmail.com>
2021-04-20 08:48:38 -0300

    Showing answers shuffled

--- assets/js/renders/renderOneQuiz.js
+++ assets/js/renders/renderOneQuiz.js
@@ -6,6 +6,32 @@ function renderOneQuiz(quiz) {
 
     banner.innerHTML = `<p>${quiz.title}</p>`;
     banner.style.backgroundImage = `url(${quiz.image})`;
+    questions.innerHTML = '';
+
+    quiz.questions.forEach(question => {
+        const shuffledAnswers = shuffleArray(question.answers);
+        let answersView = '';
+
+        shuffledAnswers.forEach(answer => {
+            let classCorrect = answer.isCorrectAnswer ? 'correct' : '';
+
+            answersView += `
+                <figure class="${classCorrect}" onclick="selectAnswer(this)">
+                    <img src="${answer.image}" />
+                    <figcaption>${answer.text}</figcaption>
+                </figure>`;
+        });
+
+        questions.innerHTML += `
+            <li class="quizz-question">
+                <div class="title" style="background-color: ${question.color};">
+                    <p>${question.title}</p>
+                </div>
+                <div class="options">
+                    ${answersView}
+                </div>
+            </li>`;
+    });
 
     showOrHide(banner, questions, myQuizzes, allQuizzes);
 }
