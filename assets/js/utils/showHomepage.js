7291bd8ab9ad1a8aa231e8f1ae313ee67970ef22
luanerdy <luan.vini1@gmail.com>
2021-04-21 04:51:39 -0300

    adjusts in showHomepage funcionalities

--- assets/js/utils/showHomepage.js
+++ assets/js/utils/showHomepage.js
@@ -2,5 +2,7 @@ function showHomepage() {
     const myQuizzes = document.querySelector('.my-quizzes');
     const allQuizzes = document.querySelector('.all-quizzes');
     const toHide = document.querySelectorAll('body > *:not(.hidden):not(header)');
-    showOrHide([myQuizzes, allQuizzes], toHide);
+
+    if(allQuizzes.classList.contains('hidden')) showOrHide([myQuizzes, allQuizzes], toHide);
+    getQuizzes();
 }
