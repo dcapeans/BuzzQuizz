078bd13c6471d748068c85b2049dfdc53d5af710
luanerdy <luan.vini1@gmail.com>
2021-04-23 17:34:41 -0300

    adjusts in user ids on post new quiz

--- assets/js/requests/postNewQuizz.js
+++ assets/js/requests/postNewQuizz.js
@@ -4,7 +4,8 @@ function postNewQuizz() {
     const promise = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/buzzquizz/quizzes", quizzObject)
     promise.then(response => {
         //salvar array ID da resposta no localStorage
-        
+        const ids = localStorage.getItem('id');
+        if(ids !== null) quizzIdArray = JSON.parse(ids);
         quizzIdArray.push(response.data.id)
         const serializedIDArray = JSON.stringify(quizzIdArray)
         localStorage.setItem("id", serializedIDArray)
